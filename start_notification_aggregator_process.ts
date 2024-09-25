7a082d3182edd879cf862a9a46d259f0c4cc2be0
Kushagra Singh Bisen <sendtokush@icloud.com>
2024-09-25 14:14:28 +0200

    added a script to start the aggregator as a node:child_process

---
+++ start_notification_aggregator_process.ts
@@ -0,0 +1,46 @@
+const { spawn } = require('child_process');
+import * as fs from 'fs';
+
+const child = spawn('npm', ['run', 'start'], {
+    stdio: 'inherit',
+    shell: true
+});
+
+
+function getTimestamp() {
+    const now = new Date();
+    return `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}-${now.getHours().toString().padStart(2, '0')}-${now.getMinutes().toString().padStart(2, '0')}-${now.getSeconds().toString().padStart(2, '0')}`;
+}
+
+interface MemoryUsage {
+    rss: number;
+    heapTotal: number;
+    heapUsed: number;
+    external: number;
+}
+
+function logCPUMemoryUsage(logFile: string) {
+    const cpuUsage = process.cpuUsage();
+    const memoryUsage: MemoryUsage = process.memoryUsage();
+    const timestamp = Date.now();
+    const logLine = `${timestamp},${cpuUsage.user},${cpuUsage.system},${memoryUsage.rss},${memoryUsage.heapTotal},${memoryUsage.heapUsed},${memoryUsage.external}\n`;
+    fs.appendFileSync(logFile, logLine);
+}
+
+const timestamp = getTimestamp();
+const logFile = `notifications-stream-aggregator-resource-usage-${timestamp}.csv`;
+fs.writeFileSync(logFile, 'timestamp,cpu_user,cpu_system,memory_rss,memory_heapTotal,memory_heapUsed,memory_external\n');
+
+setInterval(() => {
+    logCPUMemoryUsage(logFile);
+}, 500);
+
+child.on('exit', (code: any) => {
+    console.log(`Child process exited with code ${code}`);
+    process.exit(code);
+});
+
+child.on('error', (err: any) => {
+    console.log(`Child process error: ${err}`);
+    process.exit(1);
+});
